2f9180ce0aeb398e365c63f23e62c5bcb54fb3c4
hoan41210 <Hoan.uda@gmail.com>
2021-11-02 08:43:41 +0700

    bai1

---
+++ bai1.php
@@ -0,0 +1,14 @@
+<?php
+        //Enter your code here, enjoy!
+$txt = 0;
+
+for ($i=0; $i<100; $i++){
+$txt++;
+if($txt % 3 == 0 && $txt % 5 ==0)
+echo " FizzBuzz";
+    else if($txt % 3 == 0)
+    echo " Fizz";
+    else if($txt % 5 == 0)
+    echo " Buzz";
+    else echo $txt." ";
+}
